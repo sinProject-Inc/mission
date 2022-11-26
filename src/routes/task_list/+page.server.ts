43c2c60792af7b0526841041f1cb641ae14703a7
REI-MORI <morirei750@gmail.com>
2022-11-26 20:56:01 +0900

    タスク一覧を実装する #14 DBからデータを取得して画面に表示

---
+++ src/routes/task_list/+page.server.ts
@@ -0,0 +1,15 @@
+import { db } from '$lib/database/database'
+import type { Actions } from '@sveltejs/kit'
+import type { PageServerLoad } from './$types'
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+
+	const tasks = await db.task.findMany({
+		orderBy: {
+			id: 'asc',
+		},
+	})
+
+	return { tasks }
+}
---
+++ src/routes/task_list/+page.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+	import { enhance } from '$app/forms'
+	import type { PageData } from './$types'
+
+	/** @type {import('./$types').PageData} */
+	export let data: PageData
+</script>
+
+<svelte:head>
+	<title>タスク一覧</title>
+</svelte:head>
+<div>
+	<div class="outer">
+		<div class="frame glass flex_column">
+			<div class="center flex_column_logo">
+				<div class="login_text">タスク一覧</div>
+			</div>
+
+			<form method="POST" class="flex_column" use:enhance={() => {}}>
+				<ul>
+					{#each data.tasks as task}
+						<li>
+							<ul>
+								<li>{task.name}</li>
+								<li>{task.description}</li>
+								<li>{task.price}</li>
+							</ul>
+						</li>
+					{/each}
+				</ul>
+			</form>
+		</div>
+	</div>
+</div>
+
+<style>
+	:global(body) {
+		margin: 0;
+	}
+
+	input textarea {
+		width: calc(100% - 18px);
+	}
+
+	input.price {
+		width: 100px;
+		text-align: right;
+	}
+
+	button {
+		white-space: nowrap;
+	}
+
+	.error {
+		color: red;
+	}
+
+	.login_text {
+		font-size: 26px;
+	}
+
+	.outer {
+		height: 100vh;
+		padding: 0 30px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.frame {
+		padding: 30px;
+		background: #d93025;
+		width: 100%;
+		max-width: 500px;
+	}
+
+	.glass {
+		background: rgba(240, 240, 240, 0.5);
+		box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+		backdrop-filter: blur(8px);
+		-webkit-backdrop-filter: blur(8px);
+		border-radius: 8px;
+		border: 1px solid rgba(128, 128, 128, 0.25);
+		color: black;
+	}
+
+	.center {
+		text-align: center;
+	}
+
+	.buttons {
+		display: flex;
+		flex-flow: row;
+		gap: 8px;
+		align-items: start;
+		justify-content: flex-end;
+		font-size: 11pt;
+		min-height: 45px;
+	}
+
+	button:disabled {
+		color: rgb(200, 200, 200);
+	}
+
+	.flex_column_logo {
+		display: flex;
+		flex-flow: column;
+		gap: 0px;
+		margin: 6px 0;
+	}
+
+	.flex_column {
+		display: flex;
+		flex-flow: column;
+		gap: 8px;
+	}
+
+	.logo {
+		width: 70%;
+	}
+</style>
