9159f9eeb6f78a53b3479e13f575d409c376a050
REI-MORI <morirei750@gmail.com>
2023-01-25 06:22:46 +0700

    ミッションに納期を追加する #17 登録画面に納期を追加

--- src/routes/register_task/+page.svelte
+++ src/routes/register_task/+page.svelte
@@ -7,6 +7,7 @@
 	let task_input_element: HTMLInputElement
 	let description_input_element: HTMLTextAreaElement
 	let price_input_element: HTMLInputElement
+	let deadline_input_element: HTMLInputElement
 
 	onMount(() => task_input_element.focus())
 </script>
@@ -59,6 +60,13 @@
 					/>
 					<span>円</span>
 				</div>
+					<input
+						type="date"
+						bind:this={deadline_input_element}
+						name="deadline"
+						placeholder="期限"
+						required
+					/>
 				<div class="buttons">
 					<button type="submit" class="button">ミッション登録</button>
 				</div>
@@ -71,6 +79,9 @@
 </div>
 
 <style>
+	form div {
+		color: #fff;
+	}
 	input textarea {
 		width: calc(100% - 18px);
 	}
